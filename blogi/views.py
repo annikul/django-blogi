8446318562b277cf34660443dd230e42cbbf5cca
AnniK <122267866+annikul@users.noreply.github.com>
2023-02-24 12:49:13 +0200

    jatkettu djangoa

--- blogi/views.py
+++ blogi/views.py
@@ -1,6 +1,19 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+
+from.models import Postaus
+
 
 # Create your views here.
 def postaukset(request):
-    return HttpResponse('Moi!')
+    postaukset = Postaus.objects.all()  
+    context = {'postaukset': postaukset}
+    return render(                      # Rivit 10-14 voisi kaikki olla samalla rivillä
+        request, 
+        'blogi/postauslista.html',
+        context,
+    )
+
+def nayta_postaus(request, id):
+    return render(request, 'blogi/postaus.html')
+
+    
