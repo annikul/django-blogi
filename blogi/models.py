f7e748506382fd2e7d12b8ac86344d9fc50e3284
AnniK <122267866+annikul@users.noreply.github.com>
2023-03-13 13:07:12 +0200

    Ingressi

--- blogi/models.py
+++ blogi/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class Postaus(models.Model):
     otsikko = models.CharField(max_length=200)
+    ingressi = models.TextField(blank=True)
     teksti = models.TextField()
     kuva = models.ImageField(upload_to='blogi/kuvat', null=True) #(null=True meinaa että ei ole pakko olla kuvaa)
     luotu = models.DateTimeField(auto_now_add=True)
