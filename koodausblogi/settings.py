9fbf569edc2d6905b3d5cd9326cf256d70518d2b
AnniK <122267866+annikul@users.noreply.github.com>
2023-03-27 16:00:53 +0300

    Lisää DropBox-tuki settings.py

--- koodausblogi/settings.py
+++ koodausblogi/settings.py
@@ -143,6 +143,7 @@ if DROPBOX_APP_KEY:
     DEFAULT_FILE_STORAGE ='storages.backends.dropbox.DropBoxStorage'
 
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
