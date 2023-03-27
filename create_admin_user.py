f1283904e9dfd4759db5682f7acb4e8334894b79
AnniK <122267866+annikul@users.noreply.github.com>
2023-03-27 09:21:14 +0300

    korjailtu asettelua

--- create_admin_user.py
+++ create_admin_user.py
@@ -13,6 +13,14 @@ def main():
     password = os.environ['ADMIN_USER_PASSWORD']
     email = os.environ.get('ADMIN_USER_EMAIL', '')
 
+    users = get_user_model().objects
+
+    if users.filter(username-username).exists():   # Tarkistaa onko tietokannassa sillä nimellä olevaa käyttäjätunnusta 
+        print(f'User {username!r}already exists')  # ja jos on niin printataan user sillä nimellä kuin onkin
+        return                                     #{username!r} ärrä tarkoittaa että se tekee arvon siinä muodossa kuin se on koodissa
+                                                   # nyt se tarkoittaa ""
+
+
     print(f"Creating superuser {username!r}")
     get_user_model().objects.create_superuser(username, email, password)
 
