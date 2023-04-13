7d5fec902926b0d4fbadcff08e8b55d47a8e106d
AnniK <122267866+annikul@users.noreply.github.com>
2023-04-13 12:12:23 +0300

    Lisää sentry

--- koodausblogi/settings.py
+++ koodausblogi/settings.py
@@ -18,8 +18,9 @@ import dj_database_url
 SENTRY_DSN = os.environ.get('SENTRY_DSN')
 
 if SENTRY_DSN:   
-    import sentry_sdk
+    import sentry_sdk 
     from sentry_sdk.integrations.django import DjangoIntegration
+    
     sentry_sdk.init(
         dsn=SENTRY_DSN,
         integrations=[DjangoIntegration()],
